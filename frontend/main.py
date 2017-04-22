95351382d468ff7bd37e7d0fb0205fbd5a500f11
Udbhav Prasad <udbhav.prasad@bloomreach.com>
2017-04-22 15:08:54 -0700

    update for search route

--- frontend/main.py
+++ frontend/main.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def main():
-    return render_template("index.html")
+    args = request.args
+    print(args)
+    return render_template("index.html", args=args)
 
 
 if __name__ == "__main__":
